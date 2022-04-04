0761661965dc59d0a559d3bd66d8262a0c1b5e97
cobradinbalcani <adi_rossari@yahoo.com>
2022-04-04 18:39:13 +0300

    update commit

---
+++ curs_grupa_3/teme/alta_proba.py
@@ -0,0 +1,7 @@
+from proba_1 import space
+
+
+
+
+# if __name__ == '__main__':
+#     space()
---
+++ curs_grupa_3/teme/proba_1.py
@@ -0,0 +1,10 @@
+
+
+
+def space():
+    print('----spatiu------')
+
+space()
+
+if __name__ == '__main__':
+    space()
